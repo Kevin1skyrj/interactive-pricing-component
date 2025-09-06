922376e59563a2130b99e8610583c79738d18ecb
Rajat Pandey <rajatpndey257@gmail.com>
2025-09-06 19:39:12 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div suppressHydrationWarning>{children}</div>
       </body>
     </html>
   );
